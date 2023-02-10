ee02ce176eb160709883fce6f856599edae82adf
laubata2000 <laubacho2000@gmail.com>
2023-02-10 20:53:07 +0700

    dang làm man hinh cam nang
    làm chưa được

---
+++ src/CamNang.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CamNang = () => {
+  return (
+    <View>
+      <Text>CamNang</Text>
+    </View>
+  )
+}
+
+export default CamNang
+
+const styles = StyleSheet.create({})
