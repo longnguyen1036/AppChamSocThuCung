49c42e06e0e6f80d17bcaffeaa079831c237dcd7
laubata2000 <laubacho2000@gmail.com>
2023-02-11 08:56:08 +0700

    Ta commit 2 man hinh len
    mới sửa xong ngày 11/02/2023

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import CamNang from './src/CamNang'
+import ChiTietCamNang from './src/ChiTietCamNang'
+
 
 const App = () => {
   return (
     <View>
-      <CamNang/>
+      <ChiTietCamNang/>
     </View>
   )
 }
