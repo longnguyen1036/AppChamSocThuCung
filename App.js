9523c8c8a0aad2b551e33892104d0587c6b8d286
longnguyen1036 <93380209+longnguyen1036@users.noreply.github.com>
2023-02-15 12:08:05 +0700

    Long

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-<<<<<<< HEAD
+
 import Welcome from './src/screen/Welcome'
 
 const App = () => {
   return (
     <Welcome/>
-      
-   
-=======
-import CamNang from './src/CamNang'
-import ChiTietCamNang from './src/ChiTietCamNang'
-
-
-const App = () => {
-  return (
-    <View>
-      <ChiTietCamNang/>
-    </View>
->>>>>>> Ta
   )
 }
 
